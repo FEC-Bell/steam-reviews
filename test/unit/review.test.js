47f873f2d7596d941785053676fc7fe8fab43a76
Christina Wang <cywang117@outlook.com>
2020-06-24 12:37:17 +0900

    Improve data generation tests for user & review
    Ensure only 10 reviews generated per idGame, and previously untested
    user keys (idBadge, inGameId) now covered

--- test/unit/review.test.js
+++ test/unit/review.test.js
@@ -4,6 +4,7 @@ describe('Review data generation', () => {
   test('generates an array of 1000 review objects with correct data fields', () => {
     console.log = jest.fn();
     let reviewData = generateReviewData();
+    let reviewsPerGame = {};
     expect(console.log).toHaveBeenCalled();
     expect(reviewData.length).toBe(1000);
     reviewData.forEach(reviewObj => {
@@ -21,7 +22,13 @@ describe('Review data generation', () => {
         numComments: expect.any(Number),
         datePosted: expect.stringMatching(/20\d{2}-\d{2}-\d{2}/)
       });
+      if (!reviewsPerGame[reviewObj.idGame]) {
+        reviewsPerGame[reviewObj.idGame] = 0;
+      }
+      reviewsPerGame[reviewObj.idGame]++;
     });
+    expect(Object.keys(reviewsPerGame).length).toBe(100);
+    expect(Object.values(reviewsPerGame).every(val => val === 10)).toBeTruthy();
   });
 
 
--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -4,6 +4,7 @@ describe('User data generation', () => {
   test('generates an array of 750 user objects with correct data fields', () => {
     console.log = jest.fn();
     let userData = generateUserData();
+    let usernames = [];
     expect(console.log).toHaveBeenCalled();
     expect(userData.length).toBe(750);
     userData.forEach(userObj => {
@@ -17,6 +18,10 @@ describe('User data generation', () => {
         isInGame: expect.any(Boolean),
         inGameStatus: expect.any(String)
       });
+      expect(usernames).not.toContain(userObj.username);
+      usernames.push(userObj.username);
+      expect(userObj.idBadge === '' || typeof userObj.idBadge === 'number').toBeTruthy();
+      expect(userObj.inGameId === '' || typeof userObj.inGameId === 'number').toBeTruthy();
     });
   });
 });
