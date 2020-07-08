7d56fa1506d2d5ce9ab0745de41b201ba778788d
Christina Wang <cywang117@outlook.com>
2020-07-08 11:21:54 +0900

    Change mounting div id

--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReviewsModule from './src/ReviewsModule';
 
-ReactDOM.render(<ReviewsModule />, document.getElementById('app'));
+ReactDOM.render(<ReviewsModule />, document.getElementById('reviews'));
